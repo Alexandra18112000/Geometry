23af0019a416be76fd4f6606e39e10401f2f1482
Колупаева Александра Андреевна <ip814s12@mwp06.csc.local>
2019-05-06 14:28:05 +0700

    Izmenila main, make, ignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "foo.h"
+#include <iostream>
+#include <math.h>
 using namespace std;
+const float PI = 3.1415;
 int main()
 {
-    FILE *file;
-    char buffer [50];
-    file=fopen("/home/students/2018/ip814s12/Hello-world/lab1/geometry/src/circle.txt","w+t");
-    int x, y;
-    double R;
-    printf("circle(");
-    scanf ("%d %d",&x,&y);
-    printf (",");
-    scanf ("%lf",&R);
-    fprintf(file, "circle(%d %d, %1.1lf)", x, y ,R);
-    fclose(file);
-    file=fopen("/home/students/2018/ip814s12/Hello-world/lab1/geometry/src/circle.txt","r");
-    fgets (buffer,50,file);
-    printf ("%s",buffer);
-    fclose (file);
+    int a, b, c, r;
+    float p, s;
+
+    cout << "Vvedite radius kruga: ";
+    cin >> r;
+    p = PerimetrC(r, PI);
+    s = SquareC(r, PI);
+    cout << "Perimetr: " << p << " Ploshad: " << s << endl;
+
+    cout << "Vvedite storoni treugolnika (a, b, c): ";
+    cin >> a >> b >> c;
+    p = PerimetrT(a, b, c);
+    s = SquareT(a, b, c);
+    cout << endl << "Perimetr: " << p << " Ploshad: " << s;
     return 0;
 }
 
